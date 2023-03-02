51b0b1d6d4985d57a79e244921feca0fc5f909fe
Arne De Peuter <arne@depeuter.org>
2023-03-02 17:33:33 +0100

    parser

---
+++ cmake-build-debug/CMakeFiles/MetroSimDebug.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MetroSimDebug.dir/main.cpp.obj"
+  "CMakeFiles/MetroSimDebug.dir/main.cpp.obj.d"
+  "MetroSimDebug.exe"
+  "MetroSimDebug.exe.manifest"
+  "MetroSimDebug.pdb"
+  "libMetroSimDebug.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MetroSimDebug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
