27add885103472cd3cd14266d084f7deb6fe2194
cedric <cedric.devijt@gmail.com>
2023-03-02 15:13:36 +0100

    Tram classe geimplementeerd

--- Tram.cpp
+++ Tram.cpp
@@ -1,1 +1,32 @@
 #include "Tram.h"
+
+Tram::Tram(int lineNumber, int speed, Station *startStation) : lineNumber(lineNumber), speed(speed),
+                                                               startStation(startStation) {}
+
+Tram::~Tram() {
+
+}
+
+int Tram::getLineNumber() const {
+    return lineNumber;
+}
+
+void Tram::setLineNumber(int lineNumber_) {
+    Tram::lineNumber = lineNumber_;
+}
+
+int Tram::getSpeed() const {
+    return speed;
+}
+
+void Tram::setSpeed(int speed_) {
+    Tram::speed = speed_;
+}
+
+Station *Tram::getStartStation() const {
+    return startStation;
+}
+
+void Tram::setStartStation(Station *startStation_) {
+    Tram::startStation = startStation_;
+}
