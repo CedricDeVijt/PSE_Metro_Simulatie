5a7b56c14a3b3445d18a7b0d5c87939a651659b7
Arne De Peuter <arne@depeuter.org>
2023-03-07 11:27:50 +0100

    testing

--- cmake-build-debug-coverage/CMakeFiles/main_executable.dir/src/Track.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main_executable.dir/src/Track.cpp.obj: \
- C:\Users\arne\Documents\2022-2023\s2\PSE\PSE_Metro_Simulatie\src\Track.cpp \
- C:\Users\arne\Documents\2022-2023\s2\PSE\PSE_Metro_Simulatie\src\Track.h
--- tst/ParserTest.cpp
+++ tst/ParserTest.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 #include "../src/Tram.h"
 #include "../src/MetroXMLParser.h"
+#include "../src/Track.h"
+#include "../src/Line.h"
 
 class ParserTest : public ::testing::Test {
     virtual void SetUp() {}
@@ -8,10 +10,12 @@ class ParserTest : public ::testing::Test {
 };
 
 TEST_F(ParserTest, Test1) {
-    MetroXMLParser parser("xmlFiles/test.xml");
+    MetroXMLParser parser("xmlFiles/test1.xml");
     std::vector<Line*> lines = parser.getLines();
     std::vector<Tram*> trams = parser.getTrams();
     std::vector<Station*> stations = parser.getStations();
+
+    EXPECT_TRUE(parser.isProperlyInitialized());
 
     //TESTING TRAMS
     EXPECT_EQ(1, static_cast<int>(trams.size()));
@@ -21,4 +25,26 @@ TEST_F(ParserTest, Test1) {
 
     //TESTING STATIONS
     EXPECT_EQ(3, static_cast<int>(stations.size()));
+    for (int i = 0; i < static_cast<int>(stations.size()); i++) {
+        Station *station = stations[i];
+        EXPECT_EQ(12, station->getLineNumber());
+        EXPECT_TRUE(station->isProperlyInitialized());
+
+        if (station->getName()=="A") {
+            EXPECT_EQ("B", station->getNextTrack()->getAnEnd()->getName());
+            EXPECT_EQ("C", station->getPrevTrack()->getBegin()->getName());
+        } else if (station->getName()=="B") {
+            EXPECT_EQ("C", station->getNextTrack()->getAnEnd()->getName());
+            EXPECT_EQ("A", station->getPrevTrack()->getBegin()->getName());
+        } else if (station->getName()=="C") {
+            EXPECT_EQ("A", station->getNextTrack()->getAnEnd()->getName());
+            EXPECT_EQ("B", station->getPrevTrack()->getBegin()->getName());
+        }
+    }
+
+    //TESTING LINES
+    EXPECT_EQ(1, static_cast<int>(lines.size()));
+    EXPECT_EQ(12, lines[0]->getLineNumber());
+    EXPECT_EQ(3, static_cast<int>(lines[0]->getTracks().size()));
+    EXPECT_EQ(1, static_cast<int>(lines[0]->getTrams().size()));
 }
