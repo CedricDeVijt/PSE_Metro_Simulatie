8139d8df1c5bacfc9f6b71467118256bab6ef459
cedric <cedric.devijt@gmail.com>
2023-03-09 14:26:33 +0100

    SimpleOutput added

---
+++ src/SimpleOutput.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Cedric De Vijt on 09/03/2023.
+//
+
+#include "SimpleOutput.h"
+
+void SimpleOutput::outputMetroSimulation(MetroSimulation simulation) {
+    // loop over all stations
+        // format: "Station" station.getName()
+        // "<- Station ", station.getPrevTrack.getAnStart.getName()
+        // "-> Station ", station.getNextTrack.getAnEnd.getName()
+
+    // loop over all trams
+
+
+}
---
+++ src/SimpleOutput.h
@@ -0,0 +1,17 @@
+//
+// Created by Cedric De Vijt on 09/03/2023.
+//
+
+#ifndef PSE_METRO_SIMULATIE_SIMPLEOUTPUT_H
+#define PSE_METRO_SIMULATIE_SIMPLEOUTPUT_H
+
+#include "MetroSimulation.h"
+
+class SimpleOutput {
+    SimpleOutput();
+
+    void outputMetroSimulation(MetroSimulation simulation);
+};
+
+
+#endif //PSE_METRO_SIMULATIE_SIMPLEOUTPUT_H
