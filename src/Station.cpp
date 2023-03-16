b87d5098091c25d026c0dc0e60774b5c3d332215
cedric <cedric.devijt@gmail.com>
2023-03-16 14:43:36 +0100

    REQUIRE(properlyInitialized()) for Station

--- src/Station.cpp
+++ src/Station.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include "Station.h"
 #include "Track.h"
+#include "DesignByContract.h"
 
 Station::Station() {
     nextTrack = NULL;
@@ -35,15 +36,39 @@ Station::operator std::string() {
 }
 
 //getters
-const std::string &Station::getName() const { return name; }
-int Station::getLineNumber() const { return lineNumber; }
-Track *Station::getNextTrack() const { return nextTrack; }
-Track *Station::getPrevTrack() const { return prevTrack; }
+const std::string &Station::getName() const {
+    REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+    return name;
+}
+int Station::getLineNumber() const {
+    REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+    return lineNumber;
+}
+Track *Station::getNextTrack() const {
+    REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+    return nextTrack;
+}
+Track *Station::getPrevTrack() const {
+    REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+    return prevTrack;
+}
 
 //setters
-void Station::setNextTrack(Station *nextStation) { Station::nextTrack = new Track(this, nextStation); }
-void Station::setPrevTrack(Station *prevStation) { Station::prevTrack = new Track(prevStation, this); }
-void Station::setLineNumber(int lineNumber_) { Station::lineNumber = lineNumber_; }
-void Station::setName(const std::string &name_) { Station::name = name_; }
+void Station::setNextTrack(Station *nextStation) {
+    REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+    Station::nextTrack = new Track(this, nextStation);
+}
+void Station::setPrevTrack(Station *prevStation) {
+    REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+    Station::prevTrack = new Track(prevStation, this);
+}
+void Station::setLineNumber(int lineNumber_) {
+    REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+    Station::lineNumber = lineNumber_;
+}
+void Station::setName(const std::string &name_) {
+    REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+    Station::name = name_;
+}
 
 bool Station::properlyInitialized() const{ return _initCheck == this; }
