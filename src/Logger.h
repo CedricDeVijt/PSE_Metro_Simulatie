a0a4ebc4fac44e34ba7937ac1a9457c32956c70a
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2023-03-19 13:44:36 +0100

    logger Documentation

--- src/Logger.h
+++ src/Logger.h
@@ -3,8 +3,16 @@
 
 #include <iostream>
 
+/**
+ * Logger object for simple output formatting
+ */
 class Logger {
 public:
+    /**
+     * Writes an error message to the given stream, with right formatting
+     * @param stream is the stream the error message needs to be written to
+     * @param msg is the message that needs to be formatted \b !newline \b gets \b added \b by \b default!
+     */
     static void writeError(std::ostream &stream, const std::string &msg) {
         stream << "ERROR: " << msg << std::endl;
     }
