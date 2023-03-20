30b759c43598208930fac7d69ec7b29484031f9f
cedric <cedric.devijt@gmail.com>
2023-03-20 11:35:10 +0100

    MetroSimulation.h documentatie

--- src/MetroSimulation.h
+++ src/MetroSimulation.h
@@ -4,11 +4,23 @@
 
 class MetroSimulation  {
 public:
+    // Constructor
     MetroSimulation(const std::string &inputFile, std::ostream &errorstream, unsigned int runtime);
+    // Destructor
     virtual ~MetroSimulation();
 
+    /**
+     * Gives the metro-system that is used for the simulation
+     * @return
+     */
     MetroSystem *getSystem() const;
+
+    /**
+     * Updates the metro-system so all the trams will move to their next location
+     * @param os std::ostream: Output stream where the movement of the the trams gets written to
+     */
     void run(std::ostream &os);
+
 private:
     bool properlyInitialized() const;
 
