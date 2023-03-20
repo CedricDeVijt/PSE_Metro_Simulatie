91f5db570c4ff767518a2010744fbc2243dce6c1
cedric <cedric.devijt@gmail.com>
2023-03-20 11:32:35 +0100

    Tram.h documentatie

--- src/Tram.h
+++ src/Tram.h
@@ -13,24 +13,67 @@ public:
     virtual ~Tram();
 
     // Operators
+    /**
+     * Converts Tram into a string
+     * @REQUIRE(properlyInitialized(), "Station was not properly initialised.");
+     */
     operator std::string();
 
     // Getters
+    /**
+     * Gives the line number of the track on which the tram is driving
+     * @return int: line number of the track on which the tram is driving
+     */
     int getLineNumber() const;
+
+    /**
+     * Gives the speed of which the tram is capable
+     * @return int: the speed of which the tram is capable
+     */
     int getSpeed() const;
+
+    /**
+     * Gives the station where the track starts
+     * @return Station* to the station where the track starts
+     */
     Station *getStartStation() const;
+
+    /**
+     * Gives the tram number of the tram
+     * @return int: the tram number of the tram
+     */
     int getTramNumber() const;
 
+    /**
+     * Gives the station where the tram currently is
+     * @return Station* to the station where the tram currently is
+     */
     Station *getCurrentStation() const;
 
-    // Setters
-    void setLineNumber(int lineNumber);
+    /**
+     * Sets the speed of the tram
+     * @param speed int: speed of the tram
+     */
     void setSpeed(int speed);
+
+    /**
+     * Sets the station where the train starts at the start of the system
+     * @param startStation Station *: station where the tram starts form
+     */
     void setStartStation(Station *startStation);
 
+    /**
+     * Sets the tram number of the tram
+     * @param tramNumber int: the tram number given to the tram
+     */
     void setTramNumber(int tramNumber);
 
+    /**
+     * Moves the tram to the next station
+     * @param os std::ostream: where the output of moving the train is written to
+     */
     void drive(std::ostream &os);
+
 private:
     int lineNumber;
     int tramNumber;
