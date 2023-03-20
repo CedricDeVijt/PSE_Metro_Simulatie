86511bdf54fe4120ea2c646b0b4d4ff888f31a75
cedric <cedric.devijt@gmail.com>
2023-03-20 11:06:06 +0100

    MetroXMLParser.h documentatie

--- src/MetroXMLParser.h
+++ src/MetroXMLParser.h
@@ -15,11 +15,10 @@
  */
 class MetroXMLParser {
 public:
+    // Constructor
     explicit MetroXMLParser(const std::string &filename, std::ostream &errorStream);
-
+    // Destructor
     virtual ~MetroXMLParser();
-
-    bool properlyInitialized() const;
 
     /**
      * Parses the filename
@@ -32,11 +31,19 @@ public:
      */
     bool parse(const std::string& filename);
 
+    /**
+     * Parses a single station form TiXmlElement
+     * @REQUIRE properlyInitialized(), "MetroXMLParser was not initialized when calling parse"
+     * @param stationElem tinyXML element that contains information about a station
+     */
     void parseStation(TiXmlElement* stationElem);
 
+    /**
+     * Parses a single tram form TiXmlElement
+     * @REQUIRE properlyInitialized(), "MetroXMLParser was not initialized when calling parse"
+     * @param tramElem tinyXML element that contains information about a tram
+     */
     void parseTram(TiXmlElement* tramElem);
-
-    std::pair<std::string, bool> readKey(TiXmlElement* elem, const std::string &key);
 
     /**
      * Initialises the stations completely
@@ -69,10 +76,14 @@ public:
     bool verify();
 
     bool isProperlyParsed() const;
+    bool properlyInitialized() const;
+
     const std::vector<Tram *> &getTrams() const;
     const std::vector<Station *> &getStations() const;
     const std::vector<Line *> &getLines() const;
 private:
+    std::pair<std::string, bool> readKey(TiXmlElement* elem, const std::string &key);
+
     bool properlyParsed;
     MetroXMLParser *_initCheck;
     std::ostream &errorstream;
