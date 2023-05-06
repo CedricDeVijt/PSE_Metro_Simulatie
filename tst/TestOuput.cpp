751189bfbf028f61eaf27516b92cb5fcb5815c4d
CedricDeVijt <cedric.devijt2@gmail.com>
2023-05-06 20:59:26 +0200

    Output sim tests fixed

--- tst/TestOuput.cpp
+++ tst/TestOuput.cpp
@@ -9,50 +9,78 @@ class TestOutput: public ::testing::Test{
     virtual void TearDown() {}
 };
 
-TEST(TestOutput, simpleSystemOutput){
+TEST(TestOutput, simpleSystemOutput_1){
     const std::string INPUTFOLDER = "xmlFiles/tests/OutputTests/Input/";
     const std::string OUTPUTFOLDER = "xmlFiles/tests/OutputTests/Output/";
     const std::string COMPAREFOLDER = "xmlFiles/tests/OutputTests/Compare/";
+
     // create metro-system
-    MetroSystem system1;
-    MetroXMLParser::loadMetroSystem(system1, INPUTFOLDER + "simAdvanced.xml", std::cout);
-//    MetroSystem system2;
-//    MetroXMLParser::loadMetroSystem(system2, INPUTFOLDER + "simFile3.xml", std::cout);
+    MetroSystem system;
+    MetroXMLParser::loadMetroSystem(system, INPUTFOLDER + "simFile.xml", std::cout);
 
     //output streams to files
-    std::ofstream system1Output((OUTPUTFOLDER + "simpleSimFileOutput.txt").c_str());
-//    std::ofstream system2Output((OUTPUTFOLDER + "simFileOutput3.txt").c_str());
+    std::ofstream systemOutput((OUTPUTFOLDER + "simpleSystemOutput_1.txt").c_str());
 
     // output metro-systeem
-    MetroSystemOutput::simpleSystemOutput(&system1,system1Output);
-    system1Output.close();
-//    MetroSystemOutput::simpleSystemOutput(&system2,system2Output);
-//    system1Output.close();
+    MetroSystemOutput::simpleSystemOutput(&system, systemOutput);
+    systemOutput.close();
 
-    EXPECT_TRUE(FileCompare(OUTPUTFOLDER+"simFileOutput.txt", OUTPUTFOLDER+"simFileOutput.txt"));
-//    EXPECT_FALSE(FileCompare(OUTPUTFOLDER+"simFileOutput3.txt", COMPAREFOLDER+"simFileOutput3.txt"));
+    EXPECT_TRUE(FileCompare(COMPAREFOLDER+"simpleSystemOutput_1.txt", OUTPUTFOLDER+"simpleSystemOutput_1.txt"));
 }
 
-TEST(TestOutput, advancedSystemOutput){
+TEST(TestOutput, simpleSystemOutput_2){
     const std::string INPUTFOLDER = "xmlFiles/tests/OutputTests/Input/";
     const std::string OUTPUTFOLDER = "xmlFiles/tests/OutputTests/Output/";
     const std::string COMPAREFOLDER = "xmlFiles/tests/OutputTests/Compare/";
+
     // create metro-system
-    MetroSystem system1;
-    MetroXMLParser::loadMetroSystem(system1, INPUTFOLDER + "simAdvanced.xml", std::cout);
-//    MetroSystem system2;
-//    MetroXMLParser::loadMetroSystem(system2, INPUTFOLDER + "simFile3.xml", std::cout);
+    MetroSystem system;
+    MetroXMLParser::loadMetroSystem(system, INPUTFOLDER + "simAdvanced.xml", std::cout);
 
     //output streams to files
-    std::ofstream system1Output((OUTPUTFOLDER + "advancedSimFileOutput.txt").c_str());
-//    std::ofstream system2Output((OUTPUTFOLDER + "simFileOutput3.txt").c_str());
+    std::ofstream systemOutput((OUTPUTFOLDER + "simpleSystemOutput_2.txt").c_str());
 
     // output metro-systeem
-    MetroSystemOutput::advancedSystemOutput(&system1,system1Output);
-    system1Output.close();
-//    MetroSystemOutput::advancedSystemOutput(&system2,system2Output);
-//    system1Output.close();
+    MetroSystemOutput::simpleSystemOutput(&system,systemOutput);
+    systemOutput.close();
 
-    EXPECT_TRUE(FileCompare(OUTPUTFOLDER+"simFileOutput.txt", COMPAREFOLDER+"simFileOutput.txt"));
-//    EXPECT_FALSE(FileCompare(OUTPUTFOLDER+"simFileOutput3.txt", COMPAREFOLDER+"simFileOutput3.txt"));
+    EXPECT_TRUE(FileCompare(COMPAREFOLDER+"simpleSystemOutput_2.txt", OUTPUTFOLDER+"simpleSystemOutput_2.txt"));
+}
+
+TEST(TestOutput, advancedSystemOutput_1){
+    const std::string INPUTFOLDER = "xmlFiles/tests/OutputTests/Input/";
+    const std::string OUTPUTFOLDER = "xmlFiles/tests/OutputTests/Output/";
+    const std::string COMPAREFOLDER = "xmlFiles/tests/OutputTests/Compare/";
+
+    // create metro-system
+    MetroSystem system;
+    MetroXMLParser::loadMetroSystem(system, INPUTFOLDER + "simFile.xml", std::cout);
+
+    //output streams to files
+    std::ofstream systemOutput((OUTPUTFOLDER + "advancedSystemOutput_1.txt").c_str());
+
+    // output metro-systeem
+    MetroSystemOutput::advancedSystemOutput(&system, systemOutput);
+    systemOutput.close();
+
+    EXPECT_TRUE(FileCompare(COMPAREFOLDER+"advancedSystemOutput_1.txt", OUTPUTFOLDER+"advancedSystemOutput_1.txt"));
+}
+
+TEST(TestOutput, advancedSystemOutput_2){
+    const std::string INPUTFOLDER = "xmlFiles/tests/OutputTests/Input/";
+    const std::string OUTPUTFOLDER = "xmlFiles/tests/OutputTests/Output/";
+    const std::string COMPAREFOLDER = "xmlFiles/tests/OutputTests/Compare/";
+
+    // create metro-system
+    MetroSystem system;
+    MetroXMLParser::loadMetroSystem(system, INPUTFOLDER + "simAdvanced.xml", std::cout);
+
+    //output streams to files
+    std::ofstream systemOutput((OUTPUTFOLDER + "advancedSystemOutput_2.txt").c_str());
+
+    // output metro-systeem
+    MetroSystemOutput::advancedSystemOutput(&system,systemOutput);
+    systemOutput.close();
+
+    EXPECT_TRUE(FileCompare(COMPAREFOLDER+"advancedSystemOutput_2.txt", OUTPUTFOLDER+"advancedSystemOutput_2.txt"));
 }
