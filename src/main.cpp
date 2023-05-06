0005a15fe055c4ce6994264636d65849ce1a979a
ArneDePeuter <107651037+ArneDePeuter@users.noreply.github.com>
2023-05-06 12:45:02 +0200

    main use cases

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "MetroSimulation.h"
 #include "iostream"
-#include "fstream"
 #include "TramStop.h"
 #include "MetroSystemOutput.h"
 #include "MetroRenderer.h"
@@ -9,15 +8,67 @@
 const std::string INPUTPATH = "./xmlFiles/sims/input/";
 const std::string OUTPUTPATH = "./xmlFiles/sims/output/";
 
-int main() {
-    std::string filename = "simFile";
+void useCase1_1to1_4() {
+    MetroSystem sys;
+    MetroXMLParser::loadMetroSystem(sys, INPUTPATH+"simFile.xml", std::cerr);
+}
 
-    MetroSimulation sim(INPUTPATH+filename+".xml", std::cerr,10, true);
+void useCase2_2() {
+    MetroSystem sys;
+    MetroXMLParser::loadMetroSystem(sys, INPUTPATH+"simFile.xml", std::cerr);
+
+    MetroSystemOutput::simpleSystemOutput(&sys, std::cout);
+}
+
+void useCase2_3() {
+    MetroSystem sys;
+    MetroXMLParser::loadMetroSystem(sys, INPUTPATH+"simFile.xml", std::cerr);
+
+    MetroSystemOutput::advancedSystemOutput(&sys, std::cout);
+}
+
+void useCase2_4() {
+    MetroSystem sys;
+    MetroXMLParser::loadMetroSystem(sys, INPUTPATH+"simFile.xml", std::cerr);
+
+    MetroRenderer::createIni(&sys, std::cout);
+}
+
+void useCase3_1to3_8() {
+    MetroSimulation sim(INPUTPATH+"simFile.xml", std::cerr,10, false);
+
     sim.run(std::cout);
-    std::cout << std::endl;
+}
+
+void useCase3_9() {
+    MetroSimulation sim(INPUTPATH+"simFile.xml", std::cerr,10, false);
+
+    std::stringstream ignoreStream;
+    sim.run(ignoreStream);
+
     sim.evaluate(std::cout);
+}
 
-    MetroSystemOutput::advancedSystemOutput(sim.getSystem(), std::cout);
+void extraUseCase_dot() {
+    //Dot png files generated for every frame of the simulation
+    MetroSimulation sim(INPUTPATH+"simFile.xml", std::cerr,10, true);
 
-    MetroRenderer::createIni(sim.getSystem(),std::cout);
+    sim.run(std::cout);
+
+    std::cout << std::endl;
+
+    MetroSystemOutput::createDotFile(sim.getSystem(), std::cout);
+}
+
+int main() {
+//    useCase1_1to1_4();
+
+//    useCase2_2();
+//    useCase2_3();
+//    useCase2_4();
+
+//    useCase3_1to3_8();
+//    useCase3_9();
+//
+//    extraUseCase_dot();
 }
