30fe5b3319aff2744c2a816864d6658e301f7d8c
CedricDeVijt <cedric.devijt2@gmail.com>
2023-06-19 21:26:22 +0200

    Added dependency installation script

---
+++ install_GUI_tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+sudo apt-get install -y qtcreator
+sudo apt-get install -y qt6-base-dev
+sudo apt-get install -y qt-qmake
+
+sudo apt install -y qtchooser
+qtchooser -install qt6
+
+sudo apt install -y qtwayland5
+
+sudo apt-get install -y graphviz
+
